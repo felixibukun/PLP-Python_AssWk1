b34177a314245a0ebb2e6ed01b6d3a5a3a4bb834
Felix Ololade <felixibukun1010@gmail.com>
2024-11-07 16:35:59 +0100

    Add files via upload

---
+++ user_input.py
@@ -0,0 +1,9 @@
+# user_input.py
+
+# Asking the user for their name, age, and location
+name = input("Felix: ")
+age = input("23: ")
+location = input("Sapele: ")
+
+# Printing the personalized message
+print(f"Hello {name}, you are {age} years old and live in {location}.")
